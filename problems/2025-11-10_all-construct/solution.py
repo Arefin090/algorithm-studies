83ced794a8d7b7ca86c5338258ba9d0907410f97
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2025-12-17 06:10:42 +0000

    Refactor All Construct implementation

--- problems/2025-11-10_all-construct/solution.py
+++ problems/2025-11-10_all-construct/solution.py
@@ -5,7 +5,6 @@ constructed from the given list of substrings
 
 from __future__ import annotations
 
-
 def all_construct(target: str, word_bank: list[str] | None = None) -> list[list[str]]:
     """
     returns the list containing all the possible
@@ -47,7 +46,6 @@ def all_construct(target: str, word_bank: list[str] | None = None) -> list[list[
         combination.reverse()
 
     return table[len(target)]
-
 
 if __name__ == "__main__":
     print(all_construct("jwajalapa", ["jwa", "j", "w", "a", "la", "lapa"]))
