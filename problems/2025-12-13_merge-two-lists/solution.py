f645a166c142bf6870dedcae7d9fcb89069d2b86
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2025-12-22 00:13:36 +0000

    Refactor Merge Two Lists implementation

--- problems/2025-12-13_merge-two-lists/solution.py
+++ problems/2025-12-13_merge-two-lists/solution.py
@@ -10,12 +10,10 @@ from dataclasses import dataclass
 test_data_odd = (3, 9, -11, 0, 7, 5, 1, -1)
 test_data_even = (4, 6, 2, 0, 8, 10, 3, -2)
 
-
 @dataclass
 class Node:
     data: int
     next_node: Node | None
-
 
 class SortedLinkedList:
     def __init__(self, ints: Iterable[int]) -> None:
@@ -58,7 +56,6 @@ class SortedLinkedList:
         """
         return " -> ".join([str(node) for node in self])
 
-
 def merge_lists(
     sll_one: SortedLinkedList, sll_two: SortedLinkedList
 ) -> SortedLinkedList:
@@ -73,7 +70,6 @@ def merge_lists(
     True
     """
     return SortedLinkedList(list(sll_one) + list(sll_two))
-
 
 if __name__ == "__main__":
     import doctest
