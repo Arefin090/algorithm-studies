30feb03898e4bc4c2505b7560ea0c5b230121043
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-05-11 09:14:46 +0000

    Refactor Closest Pair Of Points implementation

--- problems/2026-02-14_closest-pair-of-points/solution.py
+++ problems/2026-02-14_closest-pair-of-points/solution.py
@@ -19,7 +19,6 @@ min(closest_pair_dis, closest_in_strip) would be the final answer.
 Time complexity: O(n * log n)
 """
 
-
 def euclidean_distance_sqr(point1, point2):
     """
     >>> euclidean_distance_sqr([1,2],[2,4])
@@ -27,14 +26,12 @@ def euclidean_distance_sqr(point1, point2):
     """
     return (point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2
 
-
 def column_based_sort(array, column=0):
     """
     >>> column_based_sort([(5, 1), (4, 2), (3, 0)], 1)
     [(3, 0), (5, 1), (4, 2)]
     """
     return sorted(array, key=lambda x: x[column])
-
 
 def dis_between_closest_pair(points, points_counts, min_dis=float("inf")):
     """
@@ -57,7 +54,6 @@ def dis_between_closest_pair(points, points_counts, min_dis=float("inf")):
             min_dis = min(min_dis, current_dis)
     return min_dis
 
-
 def dis_between_closest_in_strip(points, points_counts, min_dis=float("inf")):
     """
     closest pair of points in strip
@@ -77,7 +73,6 @@ def dis_between_closest_in_strip(points, points_counts, min_dis=float("inf")):
             current_dis = euclidean_distance_sqr(points[i], points[j])
             min_dis = min(min_dis, current_dis)
     return min_dis
-
 
 def closest_pair_of_points_sqr(points_sorted_on_x, points_sorted_on_y, points_counts):
     """divide and conquer approach
@@ -121,7 +116,6 @@ def closest_pair_of_points_sqr(points_sorted_on_x, points_sorted_on_y, points_co
     )
     return min(closest_pair_dis, closest_in_strip)
 
-
 def closest_pair_of_points(points, points_counts):
     """
     >>> closest_pair_of_points([(2, 3), (12, 30)], len([(2, 3), (12, 30)]))
@@ -134,7 +128,6 @@ def closest_pair_of_points(points, points_counts):
             points_sorted_on_x, points_sorted_on_y, points_counts
         )
     ) ** 0.5
-
 
 if __name__ == "__main__":
     points = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
