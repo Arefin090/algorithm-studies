39672f17ca9a220225fa8f6a6bb5ba84bf0cea1b
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-05-24 22:20:35 +0000

    Refactor Exchange Sort implementation

--- problems/2026-04-20_exchange-sort/solution.py
+++ problems/2026-04-20_exchange-sort/solution.py
@@ -20,7 +20,6 @@ def exchange_sort(numbers: list[int]) -> list[int]:
                 numbers[i], numbers[j] = numbers[j], numbers[i]
     return numbers
 
-
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
