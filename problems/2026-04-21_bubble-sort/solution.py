dcd8fd29485914e3eea193363fc3c90fd0c679f3
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-06-10 15:10:46 +0000

    Refactor Bubble Sort implementation

--- problems/2026-04-21_bubble-sort/solution.py
+++ problems/2026-04-21_bubble-sort/solution.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-
 def bubble_sort_iterative(collection: list[Any]) -> list[Any]:
     """Pure implementation of bubble sort algorithm in Python
 
@@ -59,7 +58,6 @@ def bubble_sort_iterative(collection: list[Any]) -> list[Any]:
             break  # Stop iteration if the collection is sorted.
     return collection
 
-
 def bubble_sort_recursive(collection: list[Any]) -> list[Any]:
     """It is similar iterative bubble sort but recursive.
 
@@ -110,7 +108,6 @@ def bubble_sort_recursive(collection: list[Any]) -> list[Any]:
             swapped = True
 
     return collection if not swapped else bubble_sort_recursive(collection)
-
 
 if __name__ == "__main__":
     import doctest
