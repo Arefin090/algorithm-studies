afb22a4b40bcb6260e01c0a2d0c118b96168ce00
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-07-29 10:38:27 +0000

    Refactor Bogo Sort implementation

--- problems/2026-04-05_bogo-sort/solution.py
+++ problems/2026-04-05_bogo-sort/solution.py
@@ -15,7 +15,6 @@ python bogo_sort.py
 
 import random
 
-
 def bogo_sort(collection: list) -> list:
     """Pure implementation of the bogosort algorithm in Python
     :param collection: some mutable ordered collection with heterogeneous
@@ -39,7 +38,6 @@ def bogo_sort(collection: list) -> list:
     while not is_sorted(collection):
         random.shuffle(collection)
     return collection
-
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
