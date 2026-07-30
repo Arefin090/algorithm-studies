73463de197829f3e34dc0705c1a97fb88af6e8f9
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-07-30 13:44:25 +0000

    Refactor Minimum Cost Path implementation

--- problems/2026-02-24_minimum-cost-path/solution.py
+++ problems/2026-02-24_minimum-cost-path/solution.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-
 def minimum_cost_path(matrix: list[list[int]]) -> int:
     """
     Find the minimum cost traced by all possible paths from top left to bottom right in
@@ -29,7 +28,6 @@ def minimum_cost_path(matrix: list[list[int]]) -> int:
             matrix[i][j] += min(matrix[i - 1][j], matrix[i][j - 1])
 
     return matrix[-1][-1]
-
 
 if __name__ == "__main__":
     import doctest
