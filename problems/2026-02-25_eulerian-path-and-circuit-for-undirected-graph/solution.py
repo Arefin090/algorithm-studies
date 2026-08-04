5e12fb5259213e1b920ed1c5420481fc69e6a560
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2026-08-04 04:50:15 +0000

    Refactor Eulerian Path And Circuit For Undirected Graph implementation

--- problems/2026-02-25_eulerian-path-and-circuit-for-undirected-graph/solution.py
+++ problems/2026-02-25_eulerian-path-and-circuit-for-undirected-graph/solution.py
@@ -4,7 +4,6 @@
 # time complexity is O(V+E)
 # space complexity is O(VE)
 
-
 # using dfs for finding eulerian path traversal
 def dfs(u, graph, visited_edge, path=None):
     path = (path or []) + [u]
@@ -13,7 +12,6 @@ def dfs(u, graph, visited_edge, path=None):
             visited_edge[u][v], visited_edge[v][u] = True, True
             path = dfs(v, graph, visited_edge, path)
     return path
-
 
 # for checking in graph has euler path or circuit
 def check_circuit_or_path(graph, max_node):
@@ -31,7 +29,6 @@ def check_circuit_or_path(graph, max_node):
         return 2, odd_node
     return 3, odd_node
 
-
 def check_euler(graph, max_node):
     visited_edge = [[False for _ in range(max_node + 1)] for _ in range(max_node + 1)]
     check, odd_node = check_circuit_or_path(graph, max_node)
@@ -47,7 +44,6 @@ def check_euler(graph, max_node):
         print("graph has a Euler cycle")
     path = dfs(start_node, graph, visited_edge)
     print(path)
-
 
 def main():
     g1 = {1: [2, 3, 4], 2: [1, 3], 3: [1, 2], 4: [1, 5], 5: [4]}
@@ -65,7 +61,6 @@ def main():
     check_euler(g3, max_node)
     check_euler(g4, max_node)
     check_euler(g5, max_node)
-
 
 if __name__ == "__main__":
     main()
